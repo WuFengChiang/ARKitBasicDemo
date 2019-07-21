158d6a03a7a99ef66370b6bb876136c945dd883c
wuufone <wuufone@gmail.com>
2019-07-22 00:13:25 +0800

    1. 完成基礎範例。

--- ARKitDemo/ViewController.swift
+++ ARKitDemo/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var arScnView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupArScnView()
+        setupScene()
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.arScnView.session.run(ARWorldTrackingConfiguration(), options: [.resetTracking])
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.arScnView.session.pause()
+    }
+    
+    // MARK: -
+    
+    fileprivate func setupArScnView() {
+        arScnView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+        arScnView.showsStatistics = true
+    }
+    
+    fileprivate func setupScene() {
+        arScnView.scene = SCNScene(named: "Scenes.scnassets/scene1.scn")!
+    }
 }
 
